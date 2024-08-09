fb34d836128bae0c2bd4d74429605c19a8110c31
zavaladrian <zavaladrian@gmail.com>
2024-08-09 16:33:49 -0700

    service agreement

--- app/agreement/page.tsx
+++ app/agreement/page.tsx
@@ -1,9 +1,68 @@
 import React from 'react'
 
-const AgreementPage = () => {
+
+export default function Agreement() {
   return (
-    <div>AgreementPage</div>
+    <div className="bg-white px-6 py-32 lg:px-8">
+      <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
+        <p className="text-base font-semibold leading-7 text-indigo-600">2K-Detail </p>
+        <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Service Agreement</h1>
+        <p className="mt-6 text-xl leading-8">
+        This agreement explains our obligations to you (hereafter referred to as, “Client”, “Customer”, or “you”) and 2k-detail (hereafter referred to as “2k-detail”, “contractor”, “technician”,  “us”, “our”, or “we” ) and your obligations to us in relation to the service(s) you purchase. By using our service you agree to the full terms of this agreement, without modification by you.
+        </p>
+        <div className="mt-10 max-w-2xl">
+          <p>By using the services of 2k-detail you hereby agree to the following terms:</p>
+          
+          <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">OPERATING & MOVING YOUR VEHICLE:</h2>
+          <p className="mt-6">
+          Customer will give 2k-detail access to the vehicle during the detail job and any additional mutually agreed upon times. Please make sure the keys to the vehicle are available to us. This document insures that you authorize us to operate and to move your vehicle, as necessary to do so determined by 2k-detail.
+          </p>
+          <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">
+          QUALITY OF SERVICE:</h2>
+          <p className="mt-6">
+          We want you to be satisfied with our service. No services offered are guaranteed to completely restore your vehicle to any previous or new condition. We will try our absolute best to restore the vehicle to the best condition possible and within the constraints of our resources, time, and expertise. We will conduct a final inspection together, with you, at the end of the job. If, after the final inspection and after we have left, you find our service unsatisfactory, please contact us within 24 hours so we can address your concern. 2k-detail will not be held liable for any concerns after 24 hours of job completion or if you did not complete the final inspection with us.
+          </p>
+          <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">ACCIDENTS, PRE-EXISTING CONDITION AND DAMAGE: </h2>
+          <p className="mt-6">
+          We are as careful as possible and have never once had an accident to date. We are professional and skilled at what we do. If something does get damaged while detailing your vehicle, we will advise you of the incident. By agreeing to our detailing services you also agree not to hold 2k-detail liable for any pre-existing damages to your vehicle. 
+          </p>
+          <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">FEES, TERMS OF SERVICE, AND PAYMENT TERMS</h2>
+          <p className="mt-6">
+          The fee for our service is the agreed-upon dollar amount in our communication together. The amount of time reserved is an estimate since the condition of each vehicle varies. It is possible that the job could take less or more time than the time reserved. 2k-detail has the right to charge an extra dirty fee, if the detail job takes significantly longer than expected at the discretion of 2k-detail. We will inform you of the possible fee before the detailing process.
+
+          </p>
+          <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">PROVIDING FEEDBACK AND PHOTOS</h2>
+          <p className="mt-6">
+          We ask for your feedback. Please send us an email at (2kdetailservice@gmail.com) and let us know if there are ANY concerns – good or bad. We also carry the right at the discretion of 2kdetail to use pictures of your vehicle for marketing purposes and to publish on its website (www.2k-detail.com) or any of our Social Media. Your personal information will not be shown in the photos.
+          </p>
+          <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">
+          TERMINATION OF SERVICE</h2>
+          <p className="mt-6">
+          2k-detail may cancel any new or existing maintenance plans or services with clients if we experience that the customer is not the right fit for our services at the discretion of 2k-detail. Instances of service termination include: being rude or harsh to 2k-detail employees, or any other reason we determine to be worthy of service termination by our discretion.
+          </p>
+          <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">CHANGES</h2>
+          <p className="mt-6">
+          2k-detail reserves the right to modify this statement at any time. Any changes to this Service Contract will be listed in this section. 2k-detail does not make any commitment to update the materials.
+          </p>
+          <ul role="listitem" className="mt-8 space-y-8 text-gray-600 pl-4">
+                <li className="flex gap-x-3">
+                 
+                  <span>
+                    <strong className="font-semibold text-gray-900">By using our services you hereby understand that 2k-detail does not guarantee any specific outcomes or results with our detailing services..</strong> 
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  
+                  <span>
+                    <strong className="font-semibold text-gray-900">2k-detail limits its liability to the total dollar amount of each job.​</strong> 
+                  </span>
+                </li>
+                
+              </ul>
+          <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">All Sales are Final</h2>
+          
+        </div>
+      </div>
+    </div>
   )
 }
-
-export default AgreementPage
